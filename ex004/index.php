2cc810bfcf0b4970d316fff0537c7e53bda238ad
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-02-04 17:03:08 -0300

    Tipos primitivos

---
+++ ex004/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos></h1>
+    <?php 
+        // 0x = hexadecimal 0b = binário 0 = Octal
+        // $num = 0x1A;
+        // echo "O valor da variável é $num";
+
+        // num = (integer) 3e2=(3 * 10°2) -> coerção (declarando o tipo)
+
+        // $v = (string) 300;
+        // var_dump($v); // igual typeof
+
+        // $casado = false;
+        // var_dump(($casado));
+
+        // valor true = 1 e false = vazio
+
+        // $vet = [6, 2, 9, 3, 5];
+        // var_dump($vet);
+
+        // class Pessoa {
+        //     private string $nome;
+        // }
+
+        // $p = new Pessoa;
+        // var_dump($p);
+    ?>
+</body>
+</html>
