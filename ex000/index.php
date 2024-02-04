203b890bdcad4fbea7302aebb307db8574528dd5
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-02-04 15:32:58 -0300

    Alternando branch

--- ex000/index.php
+++ ex000/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Livre</title>
 </head>
-<!--subindo para a main-->
+<!--subindo para a main!-->
 <body>
     <h1>
         <?php
