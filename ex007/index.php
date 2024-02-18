43075314f141a6a85c7a90c76d72c6a7af507dfe
VitorCruz78 <vitorluiz15012006@gmail.com>
2024-02-18 11:46:30 -0300

    Expressões aritméticas

---
+++ ex007/index.php
@@ -0,0 +1,19 @@
+<?php 
+    $res = intdiv(5, 2); //2
+    print("A resposta é $res");
+
+    $res1 = max(5, 2); //5
+    print("A resposta é $res1");
+
+    $res2 = min(5, 2); //2
+    print("A resposta é $res2");
+
+    $res3 = pi(); // ou M_PI -> 3,14...
+    print("A resposta é $res3");
+
+    $res4 = pow(5, 2); // 25 -> utilizando pow, perde a ordem de precedência da potência
+    print("A resposta é $res4");
+
+    $res5 = sqrt(81); // 9 -> raiz quadrada
+    print("A resposta é $res5");
+?>
